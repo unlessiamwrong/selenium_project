477320aa1ba09b8d17f3cd32d690dc89b5d11892
unlessiamwrong <slvbtv@gmail.com>
2022-09-02 18:07:16 +0500

    Refactor imports

--- test_main_page.py
+++ test_main_page.py
@@ -1,25 +1,39 @@
-# from .pages.main_page import MainPage
-# from .pages.login_page import LoginPage
-#
-#
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     login_page = LoginPage(browser, browser.current_url)
-#     login_page.should_be_login_page()
-#
-#
-# def test_guest_should_see_login_link(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
-#
-#
-# def test_guest_should_see_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-#     page = LoginPage(browser, link)
-#     page.open()
-#     page.should_be_login_page()
+import pytest
+
+from .pages.cart_page import CartPage
+from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
+
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        self.link = "http://selenium1py.pythonanywhere.com"
+        self.page = MainPage(browser, self.link)
+        self.page.open()
+        self.page.go_to_login_page()
+        self.login_page = LoginPage(browser, browser.current_url)
+        self.login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        self.link = "http://selenium1py.pythonanywhere.com/"
+        self.page = MainPage(browser, self.link)
+        self.page.open()
+        self.page.should_be_login_link()
+
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_cart()
+    page = CartPage(browser, link)
+    page.should_be_no_items_in_cart()
+    page.should_be_empty_message_in_cart()
